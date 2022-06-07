c698da2b4710e2a7fffe4491a46c90438cdb3151
sefeek <safeek@ontoborn.com>
2022-06-07 05:29:12 +0000

    commit to master

--- puppeteer/test.js
+++ puppeteer/test.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://8080-fcaaecbcafccfabcfbbdbadfeecdcaceefaffcabd.ide.exam.ly');
+  await page.goto('https://8080-aacdadadbbcfbbdbadfeebebbcafb.ide.exam.ly');
   console.log('TESTCASE:test_case3:success');
   await page.screenshot({path: 'example.png'});
   console.log('TESTCASE:test_case4:success');
